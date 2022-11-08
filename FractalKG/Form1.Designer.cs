a638ad41b16922de3bcddcb5381165969fe8c634
Acsious <ulchenko.f@mail.ru>
2022-11-08 10:50:46 +0300

    Добавьте файлы проекта.

---
+++ FractalKG/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace FractalKG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtAlpha = new System.Windows.Forms.TextBox();
+            this.txtBeta = new System.Windows.Forms.TextBox();
+            this.txtLmin = new System.Windows.Forms.TextBox();
+            this.txtK = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtK1 = new System.Windows.Forms.TextBox();
+            this.picFractal = new System.Windows.Forms.PictureBox();
+            this.chbClear = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbColors = new System.Windows.Forms.ComboBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picFractal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.txtAlpha, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtBeta, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtLmin, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtK, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.txtK1, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.picFractal, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.chbClear, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.cmbColors, 1, 6);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 12;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(371, 579);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // txtAlpha
+            // 
+            this.txtAlpha.Location = new System.Drawing.Point(324, 3);
+            this.txtAlpha.Name = "txtAlpha";
+            this.txtAlpha.Size = new System.Drawing.Size(35, 20);
+            this.txtAlpha.TabIndex = 0;
+            this.txtAlpha.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAlpha_KeyDown);
+            this.txtAlpha.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAlpha_KeyPress);
+            // 
+            // txtBeta
+            // 
+            this.txtBeta.Location = new System.Drawing.Point(324, 29);
+            this.txtBeta.Name = "txtBeta";
+            this.txtBeta.Size = new System.Drawing.Size(35, 20);
+            this.txtBeta.TabIndex = 1;
+            this.txtBeta.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAlpha_KeyDown);
+            this.txtBeta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAlpha_KeyPress);
+            // 
+            // txtLmin
+            // 
+            this.txtLmin.Location = new System.Drawing.Point(324, 55);
+            this.txtLmin.Name = "txtLmin";
+            this.txtLmin.Size = new System.Drawing.Size(35, 20);
+            this.txtLmin.TabIndex = 2;
+            this.txtLmin.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAlpha_KeyDown);
+            this.txtLmin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAlpha_KeyPress);
+            // 
+            // txtK
+            // 
+            this.txtK.Location = new System.Drawing.Point(324, 81);
+            this.txtK.Name = "txtK";
+            this.txtK.Size = new System.Drawing.Size(35, 20);
+            this.txtK.TabIndex = 3;
+            this.txtK.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAlpha_KeyDown);
+            this.txtK.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAlpha_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(289, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "α:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(289, 26);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "β:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(289, 52);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 26);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Lmin:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(289, 78);
+            this.label4.Margin = new System.Windows.Forms.Padding(0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 26);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "k:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(289, 104);
+            this.label5.Margin = new System.Windows.Forms.Padding(0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 26);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "k1:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtK1
+            // 
+            this.txtK1.Location = new System.Drawing.Point(324, 107);
+            this.txtK1.Name = "txtK1";
+            this.txtK1.Size = new System.Drawing.Size(35, 20);
+            this.txtK1.TabIndex = 4;
+            this.txtK1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAlpha_KeyDown);
+            this.txtK1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAlpha_KeyPress);
+            // 
+            // picFractal
+            // 
+            this.picFractal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picFractal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picFractal.Location = new System.Drawing.Point(3, 3);
+            this.picFractal.Name = "picFractal";
+            this.tableLayoutPanel1.SetRowSpan(this.picFractal, 14);
+            this.picFractal.Size = new System.Drawing.Size(283, 573);
+            this.picFractal.TabIndex = 3;
+            this.picFractal.TabStop = false;
+            // 
+            // chbClear
+            // 
+            this.chbClear.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.chbClear, 2);
+            this.chbClear.Location = new System.Drawing.Point(292, 133);
+            this.chbClear.Name = "chbClear";
+            this.chbClear.Size = new System.Drawing.Size(73, 17);
+            this.chbClear.TabIndex = 6;
+            this.chbClear.Text = "Очистить";
+            this.chbClear.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.button1, 2);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(292, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cmbColors
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.cmbColors, 2);
+            this.cmbColors.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cmbColors.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbColors.FormattingEnabled = true;
+            this.cmbColors.Items.AddRange(new object[] {
+            "Черно-белый",
+            "Радуга",
+            "Случайная радуга",
+            "Выбрать..."});
+            this.cmbColors.Location = new System.Drawing.Point(292, 156);
+            this.cmbColors.Name = "cmbColors";
+            this.cmbColors.Size = new System.Drawing.Size(76, 21);
+            this.cmbColors.TabIndex = 7;
+            this.cmbColors.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 579);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Папоротник";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picFractal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtAlpha;
+        private System.Windows.Forms.TextBox txtBeta;
+        private System.Windows.Forms.TextBox txtLmin;
+        private System.Windows.Forms.TextBox txtK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtK1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picFractal;
+        private System.Windows.Forms.CheckBox chbClear;
+        private System.Windows.Forms.ComboBox cmbColors;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+
+    }
+}
+
---
+++ FractalKG/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Reflection;
+namespace FractalKG
+{
+    public partial class Form1 : Form
+    {
+        #region Для рисования фрактала
+        const float alpha = 2;//угол закручивания
+        const float beta = 86;//угол наклона веток
+        float lmin = 1;//Минимальная длина
+        float k = 0.14F;//Частота веток
+        float k1 = 0.3F;//Длина веток
+        float cosAlpha = (float)Math.Cos(alpha * Math.PI / 180);
+        float sinAlpha = (float)Math.Sin(alpha * Math.PI / 180);
+        float cosBeta = (float)Math.Cos(beta * Math.PI / 180);
+        float sinBeta = (float)Math.Sin(beta * Math.PI / 180);
+
+        private Pen pen;//Перо для рисования
+        private Color[] rainbow = new Color[]
+        {
+            Color.Red,
+            Color.Orange,
+            Color.Yellow,
+            Color.Green,
+            Color.Blue,
+            Color.DarkBlue,
+            Color.Violet
+        };//Цвета радуги
+        private Color[] bw = new Color[]
+        {
+            Color.Black,
+            Color.White
+        };//Чёрно-белый
+        private Random rand = new Random();
+        #endregion
+
+        #region Вспомогательные
+        bool stop;//остановка рисования
+        bool notNumber;//ввод цифр в текстовые поля
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Двойная буферизация пикчербокса
+            SetDoubleBuffered(picFractal);
+            //Переменные в элементы управления
+            txtAlpha.Text = alpha.ToString();
+            txtBeta.Text = beta.ToString();
+            txtLmin.Text = lmin.ToString();
+            txtK.Text = k.ToString();
+            txtK1.Text = k1.ToString();
+            cmbColors.SelectedIndex = 1;
+
+            ToggleHandle(button1, true);
+        }
+
+        public void BuildFractal(Graphics g, PointF p1, PointF p2, int num)
+        {
+
+            Application.DoEvents();
+
+            if (stop | (((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y)) < lmin))
+                return;
+
+            //Точка p3(x3;y3)
+            PointF p3 = new PointF(
+                (p2.X - p1.X) * cosAlpha - (p2.Y - p1.Y) * sinAlpha + p1.X,
+                (p2.X - p1.X) * sinAlpha + (p2.Y - p1.Y) * cosAlpha + p1.Y);
+            //Точка p4(x4;y4)
+            PointF p4 = new PointF(
+                 p1.X * (1 - k) + p3.X * k,
+                 p1.Y * (1 - k) + p3.Y * k);
+            //Точка p5(x5;y5)
+            PointF p5 = new PointF(
+                p4.X * (1 - k1) + p3.X * k1,
+                p4.Y * (1 - k1) + p3.Y * k1);
+            //Точка p6(x6;y6)
+            PointF p6 = new PointF(
+                (p5.X - p4.X) * cosBeta - (p5.Y - p4.Y) * sinBeta + p4.X,
+                (p5.X - p4.X) * sinBeta - (p5.Y - p4.Y) * cosBeta + p4.Y);
+            //Точка p7(x7;y7)
+            PointF p7 = new PointF(
+                (p5.X - p4.X) * cosBeta + (p5.Y - p4.Y) * sinBeta + p4.X,
+                -1 * (p5.X - p4.X) * sinBeta - (p5.Y - p4.Y) * cosBeta + p4.Y);
+
+            //Выбор цвета пера
+            Color penColor = new Color();
+
+            switch (cmbColors.SelectedIndex)
+            {
+                case 0:
+                    penColor = bw[num % 2];
+                    break;
+                case 1:
+                    penColor = rainbow[num];
+                    break;
+                case 2:
+                    penColor = rainbow[rand.Next(0, 6)];
+                    break;
+                case 3:
+                    penColor = colorDialog1.Color;
+                    break;
+                default:
+                    break;
+            }
+            pen = new Pen(penColor, 1F);
+
+            g.DrawLine(pen, p4, p1);//Отрезок, который и формирует фрактал
+            BuildFractal(g, p4, p3, num);//Продолжение ствола
+            BuildFractal(g, p4, p6, num + 1);//ветка влево
+            BuildFractal(g, p4, p7, num + 1);//ветка вправо
+        }
+
+        #region Кнопка старт/стоп
+        private void Start_Click(object sender, EventArgs e)
+        {
+            stop = false;
+            ((Button)sender).Text = "Стоп";
+            ToggleHandle((Button)sender, false);
+
+            //Перенос значений из элементов управления в переменные
+            lmin = float.Parse(txtLmin.Text);
+            k = float.Parse(txtK.Text);
+            k1 = float.Parse(txtK1.Text);
+            cosAlpha = (float)Math.Cos(float.Parse(txtAlpha.Text) * Math.PI / 180);
+            sinAlpha = (float)Math.Sin(float.Parse(txtAlpha.Text) * Math.PI / 180);
+            cosBeta = (float)Math.Cos(float.Parse(txtBeta.Text) * Math.PI / 180);
+            sinBeta = (float)Math.Sin(float.Parse(txtBeta.Text) * Math.PI / 180);
+
+            Rectangle rect = picFractal.ClientRectangle;//зона рисования
+            //Очистка пикчербокса
+            if (chbClear.Checked)
+                picFractal.CreateGraphics().Clear(SystemColors.Control);
+
+            BuildFractal(picFractal.CreateGraphics(),
+                new PointF((rect.Right - rect.Left) / 2, rect.Bottom),
+                new PointF((rect.Right - rect.Left) / 2, rect.Top), 0);
+            //Возврат обработчика события на клик
+            ((Button)sender).Text = "Пуск";
+            ToggleHandle((Button)sender, true);
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            stop = true;
+            ((Button)sender).Text = "Пуск";
+            ToggleHandle((Button)sender, true);
+        }
+
+        private void ToggleHandle(Button btn, bool start)
+        {
+            if (start)
+            {
+                btn.Click -= new EventHandler(this.Stop_Click);
+                btn.Click += new EventHandler(this.Start_Click);
+            }
+            else
+            {
+                btn.Click -= new EventHandler(this.Start_Click);
+                btn.Click += new EventHandler(this.Stop_Click);
+            }
+        }
+        #endregion
+
+        #region Ввод чисел в текстбоксы
+        private void txtAlpha_KeyDown(object sender, KeyEventArgs e)
+        {
+            notNumber = false;
+            TextBox tb = (TextBox)sender;
+            //Цифры верхнего ряда клавиатуры
+            bool upperDigits = (e.KeyCode >= Keys.D0 && e.KeyCode <= Keys.D9) && e.Modifiers != Keys.Shift;
+            //Цифры цифрового блока
+            bool numPadDigits = e.KeyCode >= Keys.NumPad0 && e.KeyCode <= Keys.NumPad9;
+            //Знак минус
+            bool minus = (e.KeyCode == Keys.OemMinus || e.KeyCode == Keys.Subtract) &&
+                (tb.SelectionStart == 0 && tb.Text.IndexOf('-') == -1);
+            //Клавиши перемещения
+            bool moveKey = false;
+            switch (e.KeyCode)
+            {
+                case Keys.Delete:
+                case Keys.Back:
+                case Keys.Left:
+                case Keys.Right:
+                case Keys.Home:
+                case Keys.End:
+                    moveKey = true;
+                    return;
+                default:
+                    break;
+            }
+            notNumber = !(upperDigits | numPadDigits | minus | moveKey);
+        }
+
+        private void txtAlpha_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Запятая или точка
+            bool comma = (e.KeyChar == ',' | e.KeyChar == '.') &&
+                ((TextBox)sender).Text.IndexOfAny(new char[] { ',', '.' }) == -1;
+            e.Handled = notNumber ? !comma : notNumber;
+        }
+        #endregion
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbColors.SelectedIndex != 3) return;
+            colorDialog1.ShowDialog();
+        }
+        public static void SetDoubleBuffered(Control control)
+        {
+            // set instance non-public property with name "DoubleBuffered" to true
+            typeof(Control).InvokeMember("DoubleBuffered",
+                BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+                null, control, new object[] { true });
+        }
+    }
+}
